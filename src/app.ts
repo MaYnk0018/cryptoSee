7a03124d8d5f80b4a4a07e4a2381a235011da87c
MaYnk0018 <126932719+MaYnk0018@users.noreply.github.com>
2025-01-11 12:09:35 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,15 @@ import express from 'express';
 import {connectDB} from './config/database.js';
 import {cryptoRoutes} from './route/cryptoRoutes.js';
 import { startPriceUpdateJob } from './job/update.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 dotenv.config()
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +22,9 @@ connectDB();
 
 app.use(express.json());
 
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
 
 app.use('/', cryptoRoutes);
 
